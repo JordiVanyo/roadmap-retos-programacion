c2039a85eed8cdfe5f3726bdd1872cb74099b735
jordi vaño <jordivll12@gmail.com>
2024-01-25 15:19:56 +0100

    #00-C

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c/JordiVaño.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// https://www.learn-c.org/
+
+#define PI 3.14
+
+int main() {
+    // One single line comment
+
+    /*
+     *    Multi 
+     *    line
+     *    comment
+     */
+
+    char signedChar;
+    unsigned char unsignedChar;
+
+    short signedShort;
+    unsigned short unsignedShort;
+
+    long signedLong;
+    unsigned long unsignedLong;
+
+    int signedInt;
+    unsigned int unsignedInt;
+
+    float floatNumber;
+    double doubleNumber;
+    long double longDoubleNumber;
+
+    const int constInt = 40;
+    
+    printf("Hola C");
+
+    return 0;
+}
